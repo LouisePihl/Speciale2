04bb05706a55d46254f8ada4f1584fb9580c8e0d
Freja Lund Fredholm <frejalundfredholm@gmail.comgit config --global user.email frejalundfredholm@gmail.co>
2024-11-07 15:13:37 +0100

    Simple model

--- Simple model.R
+++ Simple model.R
@@ -7,7 +7,7 @@
 t_0 <- 41
 t_slut <- 67
 h <- 1/12  # En måned som trinlængde
-w <- 1 #start varrighed
+w <- 0 #start varrighed
 
 # Antal tidspunkter (325) svarende til månedlige trin fra 40 til 67 år
 num_months <- (t_slut-t_0)*1/h+1
@@ -25,6 +25,14 @@ Benefits_simple$Insurance_simple<-c(13220,32000,19670,5170,13510)
 
 b <- function(z) {Benefits_simple$Insurance_simple[1]*(z<=5.5/12)+Benefits_simple$Insurance_simple[2]*(5.5/12<z&z<=41.5/12)+Benefits_simple$Insurance_simple[3]*(41.5/12<z&z<=49.5/12)+Benefits_simple$Insurance_simple[4]*(49.5/12<z&z<=109.5/12)+Benefits_simple$Insurance_simple[5]*(109.5/12<z&z<=(t_slut-t_0))}
 # i år
+b_vektor <- sapply(w_values,b)
+
+#manuelt sætter ydelserne som skiller midt i en måned til gennemsnittet 
+b_vektor[6] <- 1/2*Benefits_simple$Insurance_simple[1]+1/2*Benefits_simple$Insurance_simple[2]
+b_vektor[42] <- 1/2*Benefits_simple$Insurance_simple[2]+1/2*Benefits_simple$Insurance_simple[3]
+b_vektor[50] <- 1/2*Benefits_simple$Insurance_simple[3]+1/2*Benefits_simple$Insurance_simple[4]
+b_vektor[110] <- 1/2*Benefits_simple$Insurance_simple[4]+1/2*Benefits_simple$Insurance_simple[5]
+
 
 #---------------- Intensiteter ----------------
 #filling in matrices for disability mortality and reactivation 
@@ -33,30 +41,67 @@ mu_23 <- function(t,z){exp(-8.2226723+0.0696388*t)*(z>5)+(exp(-7.0243455+0.06853
 
 integrand <- function(t){mu_21(t,w)+mu_23(t,w)}
 
-#--------------- Overgangs ssh ----------------
+#--------------- Overgangs ssh old ----------------
 
-p_22 <- function(t, z) {
-  # Funktion der beskriver integranden
-  integrand <- function(s){mu_21(s,z+s-t)+mu_23(s,z+s-t)}
+#--- Neden under er en alternativ måde at regne p_22 ved integrate ---
+#p_22 <- function(t, z) {
+  ## Funktion der beskriver integranden
+  #integrand <- function(s){mu_21(s,z+s-t)+mu_23(s,z+s-t)}
   
-  # Beregn integralet fra s til t
-  integral_result <- integrate(integrand, t, t_slut)
+  ## Beregn integralet fra s til t
+  #integral_result <- integrate(integrand, t_0, t)
   
-  # Beregn p_{22}(t, s, w, z) som eksponentialfunktionen af -integralet
-  result <- exp(-integral_result$value)
+  ## Beregn p_{22}(t, s, w, z) som eksponentialfunktionen af -integralet
+  #result <- exp(-integral_result$value)
   
-  return(result)
+  #return(result)
+  #}
+
+#p_22_values <- mapply(p_22, t = t_values, z = w_values)
+
+# Definer cashflow-funktionen, som inkluderer p_22 og b(w)
+#cashflow <- function(t,z) {p_22(t,z)*b(t)}
+
+# Beregn cashflow for hver måned
+#cashflow_values <- mapply(cashflow, t = t_values, z = w_values)
+
+
+#--------------- Overgangs ssh new ----------------
+#Denne metode benytter accumuleret sum
+integrand <- function(t, z) {
+  mu_21(t, z) + mu_23(t, z)
 }
+
+# Opret en sekvens af t og z værdier med trin h = 1/12 (månedlige trin)
+t_values <- seq(t_0, t_slut, by = h)
+z_values <- seq(w, w+t_slut-t_0, by = h)
+
+# Initialiser en vektor til at gemme resultaterne og den akkumulerede sum
+results <- numeric(length(t_values))
+accumulated_sum <- 0
+
+# Start første trin ved at evaluere ved første t og z
+t_lower <- t_values[1]
+z_lower <- z_values[1]
+
+# Loop over de resterende værdier af t og z, og akkumuler kun det nye inkrement
+for (i in 2:length(t_values)) {
+  t_upper <- t_values[i]
+  z_upper <- z_values[i]
+  
+  # Integrer kun over det lille interval fra forrige til nuværende step
+  increment <- integrand(t_upper, z_upper) * h
+  
+  # Læg inkrementet til den akkumulerede sum
+  accumulated_sum <- accumulated_sum + increment
+  results[i] <- accumulated_sum
+}
+
+p_22_vektor <- exp(-results)
 
 #--------------- Cash flow ----------------
 
-
-# Definer cashflow-funktionen, som inkluderer p_22 og b(w)
-cashflow <- function(t,z) {p_22(t,z)*b(z)}
-
-# Beregn cashflow for hver måned
-cashflow_values <- mapply(cashflow, t = t_values, z = w_values)
-
+cashflow_values <- p_22_values*b_vektor
 
 #--------------- Reserve ----------------
 library("readxl")
