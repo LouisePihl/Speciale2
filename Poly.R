f1ba38d903e8623b9524c8e9ddcc77f1bf3feebc
Freja Lund Fredholm <frejalundfredholm@gmail.comgit config --global user.email frejalundfredholm@gmail.co>
2024-11-28 07:24:43 +0100

    poly

--- Poly.R
+++ Poly.R
@@ -26,3 +26,75 @@ lines(u_seq,poly(u_seq,5)[,4],col="purple")
 lines(u_seq,poly(u_seq,5)[,5],col="orange")
 
 
+
+
+library(ggplot2)
+library(tidyr)
+
+# Data: Generer u_seq og polynomier
+u_seq <- seq(0, 3, 0.01)
+polynomials <- as.data.frame(poly(u_seq, 5))  # Beregn polynomier
+colnames(polynomials) <- paste0("Poly", 1:5)  # Navngiv kolonner
+
+# Opret data frame til ggplot
+df <- cbind(data.frame(u_seq), polynomials) %>%
+  pivot_longer(-u_seq, names_to = "Polynomial", values_to = "Value")
+
+# Farvepaletten "fall"
+fall_colors <- c("yellowgreen","blue", "steelblue", "forestgreen", "darkseagreen3")
+
+# Lav plottet
+plot_polynomials <- ggplot(df, aes(x = u_seq, y = Value, color = Polynomial)) +
+  geom_line(size = 1) +  # Plot linjerne
+  scale_color_manual(values = fall_colors) +  # Brug farvepaletten
+  labs(x = "", y = "", title = "") +  # Tilføj aksetitler
+  theme_bw() +  # Ensartet baggrund
+  theme(
+    axis.text.x = element_text(size = 14), 
+    axis.text.y = element_text(size = 14),
+    axis.title.x = element_text(size = 14),     
+    axis.title.y = element_text(size = 14),
+    legend.position = "none",
+    axis.text = element_text(size = 14),
+    text = element_text(size = 14, family = "Times New Roman", color = "black"),
+    axis.ticks = element_line(size = 0.15, color = "black"),
+    axis.ticks.length = unit(-0.15, "cm")  # Negative ticks for "indented" look
+  )
+
+
+# Print plottet
+print(plot_polynomials)
+
+# Gem plottet som en billedfil (hvis nødvendigt)
+ggsave("polybasis.png", plot = plot_polynomials, width = 6, height = 4, dpi = 300)
+
+
+# Data: Generer u_seq og polynomier
+u_seq <- seq(0, 3, 0.01)
+polynomials <- as.data.frame(poly(u_seq, 5))  # Beregn polynomier
+colnames(polynomials) <- paste0("Poly", 1:5)  # Navngiv kolonner
+
+# Opret data frame til ggplot
+df <- cbind(data.frame(u_seq), polynomials) %>%
+  pivot_longer(-u_seq, names_to = "Polynomial", values_to = "Value")
+
+# Lav plottet
+plot_polynomials <- ggplot(df, aes(x = u_seq, y = Value, linetype = Polynomial)) +
+  geom_line(color = "black", size = 1) +  # Brug sorte linjer
+  labs(x = "", y = "", title = "") +  # Tilføj aksetitler
+  theme_bw() +  # Ensartet baggrund
+  theme(
+    axis.text.x = element_text(size = 14), 
+    axis.text.y = element_text(size = 14),
+    axis.title.x = element_text(size = 14),     
+    axis.title.y = element_text(size = 14),
+    legend.position = "none",  # Fjern legenden
+    axis.text = element_text(size = 14),
+    text = element_text(size = 14, family = "Times New Roman", color = "black"),
+    axis.ticks = element_line(size = 0.15, color = "black"),
+    axis.ticks.length = unit(-0.15, "cm")  # Negative ticks for "indented" look
+  )
+
+# Print plottet
+print(plot_polynomials)
+
